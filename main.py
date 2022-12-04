893a085c32a570c0116888cffd8457bd5d41f124
bamajoe411 <bama@bamajoe411.com>
2022-12-03 19:11:39 -0500

    2nd commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     @bot.slash_command(name="hify", guild_ids=[759605199634432020, 935128289821982770])
     @option("message", description="'H'ifys your message!")
     async def hify(ctx: discord.ApplicationContext, message: str):
-        if ctx.channel_id == 937394251556356147:
+        if ctx.channel_id == 937394251556356147 or ctx.channel_id == 923430896487506002:
             new_message = message.replace("h", "***h***").replace("H", "***H***")
             await ctx.respond(f"{new_message}")
         else:
