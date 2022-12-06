b7e09f4f0bd5691fbea28b4585bbb50324817f18
bamajoe411 <bama@bamajoe411.com>
2022-12-06 14:40:24 -0500

    updates

--- main.py
+++ main.py
@@ -38,8 +38,35 @@ def main():
             new_message = message.replace("h", "***h***").replace("H", "***H***")
             await interaction.response.send_message(f"{new_message}")
         else:
-            error_msg = "You cant use this command here. must be in channel <#923430896487506002>"
+            error_msg = f"""
+            I'm sorry to inform you, <@{interaction.user.id}>, but you are not allowed
+            to use the `/hify` command here in <#{interaction.channel_id}>, because that
+            would just be downright silly! Please head on over to <#923430896487506002>
+            and use the command there as much as your heart desires.
+            """
             await interaction.response.send_message(error_msg, ephemeral=True)
+
+    @client.tree.command()
+    @app_commands.checks.has_permissions(administrator=True)
+    @app_commands.describe(
+        message='the message to send'
+    )
+    async def editmsg(interaction: discord.Interaction, message: str):
+        """Edits a message"""
+        if interaction.user.id == 196182776498749442:
+            list_channel = client.get_channel(937394251556356147)
+            list_message = await list_channel.fetch_message(1049762293585555589)
+            await list_message.edit(content=message)
+            await interaction.response.send_message("message should be edited", ephemeral=True)
+        else:
+            error_msg = f'Sorry, <@{interaction.user.id}> you are not <@196182776498749442>'
+            await interaction.response.send_message(error_msg, ephemeral=True)
+
+    @client.tree.command()
+    @app_commands.checks.has_permissions(administrator=True)
+    async def sendmsg(interaction: discord.Interaction):
+        """sends a dummy message"""
+        await interaction.response.send_message("this is a dummy message")
 
     client.run(os.getenv('TOKEN'))
 
